4875eebf4757bd0fa974d5bb90ca545fd8c1c9cb
ChaitanyaD <crdhuware07@gmail.com>
2022-07-10 14:35:49 +0530

    background

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-
-import './App.css';
-import UserForm from './components/UserForm';
+import "./App.css";
+import UserForm from "./components/UserForm";
 
 class App extends Component {
-
-  
-  
   render() {
     return (
       <div>
         <div className="col-lg-8 mx-auto text-center mt-5">
-          <h1><b>Resume Builder </b></h1>
-          <p className="lead">Please provide accurate and clear description wherever necessary.</p>
+          <h1>
+            <b>Resume Builder -by Chaitanya </b>
+          </h1>
+          <p className="lead">
+            Please provide accurate and clear description wherever necessary.
+          </p>
           <hr />
-        </div>  
-        <UserForm/>
-      </div>  
+        </div>
+        <UserForm />
+      </div>
     );
   }
 }
